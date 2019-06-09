a5877d9cc6afdfae7c34906a8aa5b1bf59b97e3a
Paul Karpukhin <ruberoid55@gmail.com>
2019-06-09 19:14:05 +0600

    Refactored questions module

---
+++ ui__base/src/main/java/me/vponomarenko/modular/navigation/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package me.vponomarenko.modular.navigation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseFragment<V : ViewModel, D : ViewDataBinding> : Fragment() {
+    abstract val viewModel: V
+    abstract val layoutId: Int
+    lateinit var binding: D
+
+    abstract fun onCreateView()
+
+    final override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = this
+        onCreateView()
+        return binding.root
+    }
+
+}
